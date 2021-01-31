703b168a6f4bbc53b17526f8bfa81d9a456aae1a
Ivan Orellana Jr <iorellana21@gmail.com>
2021-01-30 23:34:46 -0500

    working version

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-# weather-dashboard
-Weather Dashboard showing current and 5-day forecast
+# Weather Dashboard Challenge
+## Description
+Weather Dashboard app is intended for a user to enter a city name and receive information on the cities current weather and its 5 day forecast. Cities that have been searched are displayed and user will be able to select on a previous entry to see the weather conditions
+## What Was Done
+* Build HTML page
+* Build CS page
+* Build JS page
+* API calls
+* Create multiple functions
+    * restart()
+        * clear content on page and update array with city name
+    * renderCityList()
+        * display list of cities searched
+    * cityHistory()
+        * pull data from local storage
+    * currentWeatherAPI()
+        * retrieve current weather conditions
+    * uvIndexAPI()
+        * retrieve UV index info
+    * fiveDayForecastAPI()
+        * retrieve 5 day forecast
+
+---
+## Process
+User enters a city name in the input field to see weather conditions.
+* List of cities searched is shown
+* Current weather is shown with UV index data
+* 5 day forecast is shown
+* User can click on cities that have been searched to see weather conditions again
+
+
+## Main Page
+![alt text](pic "weather-main-page")
+
+---
+## Credits
+* [Stack Overflow](https://stackoverflow.com/)
+* [W3Schools](https://www.w3schools.com/)
+* [Google](https://www.google.com/)
+
+---
+## Links
+#### Live URL
+* https://iorellana21.github.io/weather-dashboard/
+#### GitHub URL
+* https://github.com/iorellana21/weather-dashboard
--- script.js
+++ script.js
@@ -1,53 +1,223 @@
-// get data from local storage - searched cities to appear from recent to old on page
-var searchedCities = localStorage.getItem("city-list")
+var cityName = $("city-input");
+var citySearchList = $("#city-list");
+var currentWeather = $("#current-weather");
+var uvIndex = "";
+var cityNameTag = $("<h3>");
+var lat = "";
+var long = "";
+var latLong = "";
+var cityArray = [];
+var apiKey = "c7cde66d595fb98577da25bd96a3df85";
 
-// what should happen if searchCities is empty
-if (searchedCities === null) {
-    // do nothing and leave it as an empty array
-    searchedCities = [];
-}
-// what happens if searchCities has data - aka data in localStorage
-else {
-    // parse string into an object - ??? understand this more
-    searchedCities = JSON.parse(searchedCities);
-}
+// function to start from scratch and update cityArray with user input
+function restart() {
+    console.log("restart function");
 
-// what happens when Search button is clicked
-$("#search-city").on("click", function (event) {
-    // show search button
-    console.log(event.target);
-    // select city input field
-    var cityInput = document.querySelector("#city-input");
-    
-    // take user input, push into array, save to local storage
-    searchedCities.push(cityInput.value);
-    localStorage.setItem("city-list", JSON.stringify(searchedCities));
+    // empty currentWeather div tag
+    $(currentWeather).text("");
+    // empty 5day-forecast span tags
+    document.getElementById("day7").innerHTML = "";
+    document.getElementById("day15").innerHTML = "";
+    document.getElementById("day23").innerHTML = "";
+    document.getElementById("day31").innerHTML = "";
+    document.getElementById("day39").innerHTML = "";
 
-    // use function to display city search history onto page
-    renderCityList();
-});
-
-// function to display list of searched cities
-function renderCityList() {
-    // select element where buttons are going to be added
-    var cityList = $("#city-list");
-
-    // looping thru each item in searchedCities array
-    for(var i = 0; i < searchedCities.length; i++){
-        // display list of searched cities
-        console.log(searchedCities[i]);
-
-        // create button for each item, add class and city name
-        var button = $("<button>").addClass("city-button").text(searchedCities[i]);
-        // create line break
-        var line = $("<br>");
+    // assign user input to cityName variable
+    cityName = $("#city-input").val();
+    // stop if user searches for blank value
+    if (cityName === "") {
+        return;
     }
 
-    // append button to cityList
-    $(cityList).append(line);
-    $(cityList).prepend(button);
-    $(cityList).prepend(line);
+    // add user input to cityArray
+    cityArray.push(cityName);
+    // empty input tag
+    $("#city-input").innerHTML = "";
 }
 
-// calling function to display searchedCities
-renderCityList();
+// function to create li & button tags and display cities searched
+function renderCityList() {
+    console.log("renderCityList function");
+    // assign cityName to variable
+    cityName = $("#city-input").val();
+    console.log("city searched: " + cityName);
+
+    // create li tags
+    var listTag = $("<li>");
+    // assigning list class
+    $(listTag).attr("class", "list");
+    // create button tag
+    // var cityButtons = $("<button>");
+
+    // assigning cityName to button tag
+    $(listTag).text(cityName);
+
+    // append button to li
+    // listTag.append(cityButtons);
+
+    // append li tag to page
+    citySearchList.append(listTag);
+}
+
+// function to get city history from local storage
+function cityHistory() {
+    // retrieve city history from local storage
+    var cityHistoryList = JSON.parse(localStorage.getItem("city-history-list"));
+
+    // check if local storage has data
+    if (cityHistoryList !== null) {
+        console.log("cityHistory function");
+        // append data to array
+        cityArray = cityHistoryList;
+    } else {
+        return;
+    }
+
+    // create variable to retrieve values from local storage
+    var keys = Object.entries(cityHistoryList);
+    // go through each value in local storage
+    keys.forEach(([key, value]) => {
+        // create li tag
+        var listTag = document.createElement("li");
+        // assign value to li tag
+        listTag.textContent = value;
+        // assign class
+        listTag.setAttribute("class", "list");
+        // append to unordered list
+        citySearchList.append(listTag);
+    });
+}
+
+// function for current weather API call
+function currentWeatherAPI() {
+    console.log("currentWeatherAPI function");
+    console.log("uvIndexAPI function");
+
+    // add border and spacing for currentWeather div tag
+    currentWeather.addClass("current-weather");
+
+    // assign cityName to h3 tag
+    $(cityNameTag).text(cityName);
+    // append h3 tag to currentWeather div tag
+    currentWeather.append(cityNameTag);
+
+
+    // query URL for city weather
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
+
+    // API call for city weather
+    $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+        .then(function(response) {
+            // variable to get icon for todays weather
+            var todayIcon = response.weather[0].icon;
+            // assign value to todayIconURL
+            var todayIconURL = "https://openweathermap.org/img/wn/" + todayIcon + ".png";
+            // append todayIconURL next to cityNameTag
+            $(cityNameTag).append("<img src =" + todayIconURL + ">");
+
+            // variable to get temp in F
+            var f = Math.floor((response.main.temp - 273.15) * 1.8 + 32);
+            // append temp to currentWeather
+            $(currentWeather).append("Temperature: " + f + "&deg; F <br>");
+
+            // variable to contain humidity and wind speed
+            var humidity = "Humidity: " + response.main.humidity + "%";
+            var windSpeed = "Wind Speed: " + response.wind.speed + " MPH";
+            // append humidity and wind speed to currentWeather
+            $(currentWeather).append(humidity + "<br />" + windSpeed + "<br>");
+
+            // create variables to contain lat and long of searched city to get UV index
+            lat = response.coord.lat;
+            long = response.coord.lon;
+            latLong = "&lat=" + lat + "&lon=" + long;
+
+            // call uvIndex function
+            uvIndexAPI();
+
+        });
+}
+
+// function for UV index API call
+function uvIndexAPI() {
+    // create query URL for UV index
+    var queryURL = "https://api.openweathermap.org/data/2.5/uvi/forecast?appid=" + apiKey + latLong;
+
+    // API call for UV index
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+
+        // create variable to get uvIndex
+        uvIndex = response[0].value;
+        // assign text to currentWeather
+        $(currentWeather).append("UV Index: ");
+
+        // check uvIndex, create span tags and assign appropriate color
+        if (uvIndex < 3) {
+            $("<span>", { class: "uvGreen", html: uvIndex }).appendTo(currentWeather);
+        } else if (uvIndex > 3 && uvIndex < 7) {
+            $("<span>", { class: "uvYellow", html: uvIndex }).appendTo(currentWeather);
+        } else {
+            $("<span>", { class: "uvRed", html: uvIndex }).appendTo(currentWeather);
+        }
+    });
+}
+
+// function for 5day-forecast API call
+function fiveDayForecastAPI() {
+    console.log("fiveDayForecast function");
+    // query URL for five day forecast
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + apiKey;
+
+    // API call for 5day-forecast
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        // for-loop for 5day forecast
+        for (var i = 7; i < response.list.length; i += 8) {
+            $("#day" + i).addClass("five-day-forecast");
+            // convert date from unix time stamp to mm/dd/yyyy format
+            var unixTime = new Date(response.list[i].dt * 1000).toLocaleDateString(
+                "en-US"
+            );
+
+            // variable to contain date
+            var date = "<strong>" + unixTime + "</strong>";
+            // variable to select weather icon
+            var icon = response.list[i].weather[0].icon;
+            // variable to contain weather icon
+            var iconURL = "<img src=https://openweathermap.org/img/wn/" + icon + ".png> <br>";
+            // variable to get temp in K
+            kelvin = response.list[i].main.temp;
+            // convert temp to fahrenheit
+            var temp = "Temp: " + Math.floor((kelvin - 273.15) * 1.8 + 32) + "&deg; F <br>";
+            // variable to contain humidity
+            var humidity = "Humidity: " + response.list[i].main.humidity + "%";
+            // append info to appropriate forecast date
+            $("#day" + i).append(date, iconURL, temp, humidity);
+        }
+    });
+}
+
+// what happens when search icon button has been clicked
+$("#search-button").on("click", function(event) {
+    console.log("search has kicked off");
+    event.preventDefault();
+
+    // store data in userArray to localStorage
+    localStorage.setItem("city-history-list", JSON.stringify(cityArray));
+
+    // call functions
+    restart();
+    renderCityList();
+    currentWeatherAPI();
+    fiveDayForecastAPI();
+});
+
+// call cityHistory function to display city-history-list
+cityHistory();
